1a5ce9c53c415dc9af38e01c51b3611b6935f16a
alvaro <alvarord519@gmail.com>
2023-12-11 11:12:53 -0500

    Added signOut Button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,10 +4,20 @@ import { LogOut, PanelLeft } from "lucide-react";
 import { Button } from "./ui/button";
 import MainNav from "./main-nav";
 import { usePanelStore } from "@/store/useStore";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
   const isPanelOpen = usePanelStore((state) => state.isPanelOpen)
   const togglePanel = usePanelStore((state) => state.togglePanel)
+
+  const supabase = createBrowserSupabaseClient();
+  const router = useRouter()
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut()
+    router.refresh()
+  }
 
   return (
     <aside className={`fixed ${isPanelOpen ? 'w-60' : 'w-20'} top-0 left-0 bg-background h-full text-primary border-r-2 flex flex-col flex-grow transition-all`}>
@@ -33,7 +43,7 @@ const Navbar = () => {
       <footer className="flex flex-grow items-end py-3 px-4">
         <Button variant={'link'} className="flex gap-2 p-0 hover:text-indigo-500">
           <LogOut />
-          <span className={`${isPanelOpen ? '' : 'hidden'}`}>Cerrar Sesion</span>
+          <span onClick={handleSignOut} className={`${isPanelOpen ? '' : 'hidden'}`}>Cerrar Sesion</span>
         </Button>
       </footer>
     </aside>
